a7c93790e8c6ff634c35c15de6987ea76f0e914e
jepidoptera <jepidoptera@live.com>
2019-02-14 08:13:59 -0600

    initial commit

---
+++ scripts/fight.js
@@ -0,0 +1,5 @@
+// character 0 : berserker: attack power increases each time
+// character 1 : antimage : nullifies other characters' powers
+// character 2 : knight : shield
+// character 4 : cleric : heals
+
